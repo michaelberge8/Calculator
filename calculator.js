0783f5e5d5b564af906fdad08d90cf4f1cc90d17
Michael Berge <22512641+michaelberge8@users.noreply.github.com>
2019-11-05 01:23:57 -0800

    Add files via upload
    Main project files for calculator

---
+++ calculator.js
@@ -0,0 +1,10 @@
+function getOutput() {
+    return document.getElementById("output-value").innerText;
+}
+
+function printOutput(num) {
+    if(num==="")
+        document.getElementById("output-value").innerText = num;
+    else
+        document.getElementById("output-value").innerText = num;
+}
