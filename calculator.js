d6f4658743e0237f41824e6539d6171f1744b8ef
Michael Berge <22512641+michaelberge8@users.noreply.github.com>
2019-11-05 16:19:23 -0800

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -8,3 +8,42 @@ function printOutput(num) {
     else
         document.getElementById("output-value").innerText = num;
 }
+
+function getConfettiButtonOutput() {
+    return document.getElementById("confetti").innerText;
+}
+
+function validLength() {
+    return output.length <= 12;
+}
+
+function isFloat(n) {
+    return n % 1 !== 0;
+}
+
+let number = document.getElementsByClassName("number");
+for(let i=0; i<number.length; i++){
+    number[i].addEventListener('click',function(){
+        output = getOutput();
+        if(output === "0" && !equals && run && validLength()) {
+            equals = false;
+            op = false;
+            output = "";
+            output += this.id;
+            printOutput(output);
+        }
+        else if(equals && run && validLength()) {
+            equals = false;
+            op = false;
+            output = "";
+            output += this.id;
+            printOutput(output);
+        }
+        else if(output !== "" && !equals && run && validLength()) {
+            equals = false;
+            op = false;
+            output += this.id;
+            printOutput(output);
+        }
+    });
+}
